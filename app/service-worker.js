73b0347b7fccd252b8fe079cdd3cbb314dbed1b1
RedAkaAguilar <pandagamer7553@outlook.com>
2025-08-21 22:21:36 -0600

    Update service-worker.js

--- app/service-worker.js
+++ app/service-worker.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   "./",
   "./home.html",
   "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
+  "./icon/icon-192.png",
+  "./icon/icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
 ];
 
